99abe50c8ab23b2ab2c19ef73bb94d076333a75b
JEFERSON RODRIGUES DA SILVA <jeferson.rsilva9@senacsp.edu.br>
2025-03-19 22:06:45 -0300

    menu hamburguer

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+// FECHA MENU APÓS CLICAR NAS OPCÇÕES DO MENU SANDUICHE
+const header = document.querySelector('#mobile-navbar [href="#header"]' );
+const exprtise = document.querySelector('#mobile-navbar [href="#expertise-areas"]' );
+const about = document.querySelector('#mobile-navbar [href="#about"]' );
+const team = document.querySelector('#mobile-navbar [href="#team"]' );
+const contact = document.querySelector('#mobile-navbar [href="#contact"]' );
+
 // SELECIONAR OS ELEMENTOS DOS SLIDES
 const slides = document.querySelectorAll(".banner");
+
 
 const dots = document.querySelectorAll(".dot");
 
 // VARIAVES DE CONTROLE 
-let slidesIndex = 0;
+let slidesIndex = 1;
 
 function showSlides(){
+    // console.log(slides)
     for(let i=0; i < slides.length; i++){
         slides[i].classList.remove("active")
         dots[i].classList.remove("active")
@@ -22,6 +35,47 @@ function showSlides(){
       
     setTimeout(showSlides, 3000)
 }
+
+
+// ATIVAR MENU MOBILE 
+
+// ARROW FUNCTION
+// () = > {
+// }
+
+// menuBtn.addEventListener("click", (e) =>{
+//     menu.classList.add("menu-active")
+// } )
+
+// closeMenuBtn.addEventListener("click", (e) =>{
+//     menu.classList.remove("menu-active")
+// } )
+
+[menuBtn, closeMenuBtn].forEach ((btn) => {
+    btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-active")
+    })
+})
+
+// about.addEventListener("click", (e) =>{
+//     menu.classList.remove("menu-active")
+// } )
+
+// header.addEventListener("click", (e) =>{
+//     menu.classList.remove("menu-active")
+// } )
+
+// expertise.addEventListener("click", (e) =>{
+//     menu.classList.remove("menu-active")
+// } )
+
+// team.addEventListener("click", (e) =>{
+//     menu.classList.remove("menu-active")
+// } )
+// contact.addEventListener("click", (e) =>{
+//     menu.classList.remove("menu-active")
+// } )
+
 
 // INICIALIZAÇÃO 
 showSlides()
